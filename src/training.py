9e8b854a52a4108fef8032c577ca23d5eea6e5c5
ravikanur <ravikanur@gmail.com>
2021-11-20 20:18:20 +0530

    Chages

--- src/training.py
+++ src/training.py
@@ -2,8 +2,10 @@ from utils.common import read_config
 from utils.data_mgmt import get_data
 from utils.model import create_model
 from utils.model import save_model
+from utils.common import save_plot
 
 import argparse
+import pandas as pd
 
 import os
 
@@ -22,15 +24,23 @@ def training(config_path):
     artifacts_dir = config["artifacts"]["artifacts_dir"]
     model_dir = config["artifacts"]["model_dir"]
     model_name = config["artifacts"]["model_name"]
+    plots_dir = config["artifacts"]["plots_dir"]
+    plot_name = config["artifacts"]["plot_name"]
+    log_dir = config["logs"]["logs_dir"]
 
     model_dir_path = os.path.join(artifacts_dir, model_dir)
     os.makedirs(model_dir_path, exist_ok=True)
 
+    plot_dir_path = os.path.join(artifacts_dir, plots_dir)
+    os.makedirs(plot_dir_path, exist_ok=True)
+
     model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS)
 
     history = model.fit(X_train, y_train, validation_data=(X_valid, y_valid),epochs=EPOCHS, verbose=True)
+    history_data = pd.DataFrame(history.history)
 
     save_model(model, model_name, model_dir_path)
+    save_plot(history_data, plot_dir_path, plot_name)
 
 
 if __name__ == '__main__':
--- src/utils/common.py
+++ src/utils/common.py
@@ -1,8 +1,21 @@
 import yaml
+import matplotlib.pyplot as plt
+import time
+
+import os
 
 
 def read_config(config_path):
-    with  open(config_path) as config_file:  # command to open the file
+    with open(config_path) as config_file:  # command to open the file
         content = yaml.safe_load(config_file)  # to load the data of config file
 
     return content
+
+
+def save_plot(history_data, plot_dir_path, plot_name):
+    unique_name = time.strftime(f"{plot_name}_%Y%m%d_%H%M%S.png")
+    history_data.plot(figsize=(10, 10))
+    plt.grid
+
+    plot_path = os.path.join(plot_dir_path, unique_name)
+    plt.savefig(plot_path)
